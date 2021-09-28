939a22e6635e0a0d607997fc20b0f1747bd2e047
AprianDraniIrenius12200897 <90883650+AprianDraniIrenius12200897@users.noreply.github.com>
2021-09-28 10:23:46 +0700

    Tugas Pertemuan 2

---
+++ daftar_member.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Daftar Member</title>
+    </head>
+    <body>
+        <h1>Hai... kame mau menjadi member?</h1>
+        <p1>Isikan data diri kamu dulu yah agar kami kenal dengan memeber kami</p1>
+    </body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Perputakaan Login</title>
+</head>
+<body>
+    
+    <h1>Selamat Datang di Sistem Informasi Perpustakan</h1>
+    <p>
+        ini adalah layanan perpustakaan digital masa kini
+        <a href="<?=base_url( relativePath:'/daftar-member')?>">klik daftar</a> untuk menjadi member
+    </p>
+
+</body>
+</html>
