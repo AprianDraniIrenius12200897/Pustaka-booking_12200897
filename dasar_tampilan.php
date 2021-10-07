f689e9e4202bec67f6f1e16fe92cf5379ef8c5f5
AprianDraniIrenius12200897 <90883650+AprianDraniIrenius12200897@users.noreply.github.com>
2021-10-07 12:40:09 +0700

    Tugas Pertemuan 3

---
+++ dasar_tampilan.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title></title>
+        <link href="<?=base_url('/assets/css/bootstrap.min.css')?>" rel="stylesheet" />
+    </head>
+    <body>
+
+        <?=$this->renderSection('isiweb')?>
+        
+        <script src="<?=base_url('/asset/js/bootstrap.bundle.min.js')?>"></script>
+
+    </body>
+</html>
