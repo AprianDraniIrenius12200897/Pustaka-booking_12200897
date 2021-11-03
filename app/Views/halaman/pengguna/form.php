b36fa2699224bbbe0eb8b85f6c0d0c7bd912093a
ireniusaprian29@Gmail.com <ireniusaprian29@Gmail.com>
2021-11-03 21:35:03 +0700

    Tugas Pertemuan 6 CRUD

--- app/Views/halaman/pengguna/form.php
+++ app/Views/halaman/pengguna/form.php
@@ -3,69 +3,71 @@
 
 use Config\Services;
 
-    $vd = $vd ?: Services::validation();
-
+$vd = $vd ?: Services::validation();
 ?>
-<?$this->section('konten')?>
+
+<?=$this->section('konten')?>
     <div class="container">
+        <div class="col-md-5">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Form Pelanggan</h3>
+                </div>
+                <div class="card-body">
 
-        <div class="col-md;5">
-            <div class='card-body'>
-                        <form method="post" action="/pengguna">
-                    <div>
-                <div class='card'>
-                    <div class='card-header'>
-                        <h3 class="card-title">Form Pelanggan</h3>
-                    </div>
-                        <form method='post' action='/pengguna'>
-                            <input type="hidden" name="id" value"<?=$data('id') ?? ''?>" />
-                            
-                            <?php if(data('id')): ?>
-                                <input type="hidden" name="_method" value="patch" />
-                            <?php endif; ?>
-                            <div>
-                                <label for='txtNama' class="form-table">Nama</label>
-                                <input id='txtNama' type="text" name="nama" value="<?=$data('nama')?>" class="form-control" />
-                                <?php
-                                    if( $vd->getError('nama') ){?>
-                                        <div class ='alert alert-danger'>
-                                            <?=$vd->getError('nama')?>
-                                        </div>
-                                <?php }
-                                ?>
-                            </div>
-                            
-                            <div>
-                                <label for='txtPassword' class="form-table">KataSandi</label>
-                                <input id='txtPassword' type="password" name="password" value="" class="form-control" />
-                                <?php
-                                    if( $vd->getError('password') ){?>
-                                        <div class ='alert alert-danger'>
-                                            <?=$vd->getError('password')?>
-                                        </div>
-                                <?php }
-                                ?>
-                            </div>
+                    <?php if ($error):?>
+                        <div class="alert alert-danger">
+                            <?=$error?>
+                        </div>
+                    <?php endif;?>
 
-                            <div>
-                                <label for='txtPassword2' class="form-table">Ulangi Kata Sandi</label>
-                                <input id='txtPassword2' type="password" name="password2" value="" class="form-control" />
-                                <?php
-                                    if( $vd->getError('password2') ){?>
-                                        <div class ='alert alert-danger'>
-                                            <?=$vd->getError('password2')?>
-                                        </div>
-                                <?php }
-                                ?>
-                            </div>
+                    <form action="/pengguna" method="post">
+                        <input type="hidden" name="id" value="<?=$data['id'] ?? ''?>">
 
-                            <br/>
-                            <button class="btn btn-sm btn-primary">Simpan</button>
-                        </form>
-                    </div>
+                        <?php if( isset ($data['id']) ): ?>
+                            <input type="hidden" name="_method" value="patch">
+                        <?php endif;?>
+
+                        <div>
+                            <label for="txtNama" class="form-label">Nama</label>
+                            <input type="text" name="nama" value="<?=$data['nama'] ?? ''?>" class="form-control" id="txtNama">
+                        <?php
+                            if ($vd->getError('nama')) {?>
+                                <div class="alert alert-danger">
+                                    <?=$vd->getError('nama')?>
+                                </div>
+                        <?php }
+                        ?>
+                        </div>
+                        <div>
+                            <label for="txtPassword" class="form-label">Kata Sandi</label>
+                            <input type="password" name="password" value="" class="form-control" id="txtPassword">
+                        <?php
+                            if ($vd->getError('password')) {?>
+                                <div class="alert alert-danger">
+                                    <?=$vd->getError('password')?>
+                                </div>
+                        <?php }
+                        ?>
+                        </div>
+                        <div>
+                            <label for="txtPassword2" class="form-label">Ulangi Kata Sandi</label>
+                            <input type="password" name="password2" value="" class="form-control" id="txtPassword2">
+                        <?php
+                            if ($vd->getError('password2')) {?>
+                                <div class="alert alert-danger">
+                                    <?=$vd->getError('password2')?>
+                                </div>
+                        <?php }
+                        ?>
+                        </div>
+
+                        <br>
+                        <button class="btn btn-sm btn-primary">Simpan</button>
+                    </form>
                 </div>
             </div>
         </div>
-    
     </div>
-<?=$this->endSection()?>
+
+<?=$this->endSection()?> 
